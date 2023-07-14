cf1d5b76045cdbb6f5001a91c545ec04d466f343
akk1029 <96523229+akk1029@users.noreply.github.com>
2023-07-14 14:09:50 +0800

    Update darkmode.js

--- Website/js/darkmode.js
+++ Website/js/darkmode.js
@@ -3,8 +3,8 @@ function changeTheme() {
     var mode = document.getElementsByTagName('link')[2];
     var image = document.getElementsByTagName('img')[1];
 
-    // Change the value of href attribute
-    // to change the css sheet.
+    // Change the value of href attribute to change the css sheet.
+    // change the value of src to change the colormode icon
     if (mode.getAttribute('href') == 'css/lightmode.css') {
         mode.setAttribute('href', 'css/darkmode.css');
         image.setAttribute('src', 'images/darkmode.png');
